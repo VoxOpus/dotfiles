abbdd27153bc1c826a210c4c34fde0f5d4d643df
VoxOpus <nathanieldenisludtke@gmail.com>
2026-07-03 00:05:50 +0200

    init: setup dotfiles base

---
+++ termux/zsh_aliases/.zsh_aliases.zsh
@@ -0,0 +1,2 @@
+alias update='pkg update && pkg upgrade -y'
+alias ubuntu='proot-distro login ubuntu --bind ~/dotfiles:/root/dotfiles --bind ~/.ssh:/root/.ssh'
---
+++ ubuntu/zsh_aliases/.zsh_aliases.zsh
@@ -0,0 +1,3 @@
+alias update='apt update && apt upgrade -y'
+alias start-webui='source ~/open-webui-venv/bin/activate && ~/open-webui-venv/bin/open-webui serve'
+alias start-typingmind='cd typingmind && npm run start'
